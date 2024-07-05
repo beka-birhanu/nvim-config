32dde9ba943307692895de4f0cb7620b77a36c3e
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-05 18:23:20 +0300

    config: update keymap at ./init.lua

--- init.lua
+++ init.lua
@@ -1,46 +1,51 @@
-require "romareo.plugins.utils.launch"
-require "romareo.core.options"
-require "romareo.core.keymaps"
-require "romareo.plugins.autohelpers.autocmds"
+require("romareo.plugins.utils.launch")
+require("romareo.core.options")
+require("romareo.core.keymaps")
+require("romareo.plugins.autohelpers.autocmds")
 
 --UI
-spec "romareo.plugins.ui.colorscheme"
-spec "romareo.plugins.ui.devicons"
-spec "romareo.plugins.ui.breadcrumbs"
-spec "romareo.plugins.ui.illuminate"
-spec "romareo.plugins.ui.lualine"
-spec "romareo.plugins.ui.navic"
-spec "romareo.plugins.ui.dashboard"
+spec("romareo.plugins.ui.colorscheme")
+spec("romareo.plugins.ui.devicons")
+spec("romareo.plugins.ui.breadcrumbs")
+spec("romareo.plugins.ui.illuminate")
+spec("romareo.plugins.ui.lualine")
+spec("romareo.plugins.ui.navic")
+spec("romareo.plugins.ui.dashboard")
+spec("romareo.plugins.ui.markdown")
+spec("romareo.plugins.ui.images")
 
 -- autohelpers
-spec "romareo.plugins.autohelpers.treesitter"
-spec "romareo.plugins.autohelpers.lspconfig"
-spec "romareo.plugins.autohelpers.cmp"
-spec "romareo.plugins.autohelpers.none-ls"
-spec "romareo.plugins.autohelpers.mason"
-spec "romareo.plugins.autohelpers.schemastore"
-spec "romareo.plugins.autohelpers.autoclose"
-spec "romareo.plugins.autohelpers.comment"
-spec "romareo.plugins.autohelpers.oil"
-spec "romareo.plugins.autohelpers.ufo"
+spec("romareo.plugins.autohelpers.treesitter")
+spec("romareo.plugins.autohelpers.lspconfig")
+spec("romareo.plugins.autohelpers.cmp")
+spec("romareo.plugins.autohelpers.mason")
+spec("romareo.plugins.autohelpers.schemastore")
+spec("romareo.plugins.autohelpers.autoclose")
+spec("romareo.plugins.autohelpers.comment")
+spec("romareo.plugins.autohelpers.oil")
+spec("romareo.plugins.autohelpers.ufo")
+spec("romareo.plugins.autohelpers.lint")
+spec("romareo.plugins.autohelpers.conform")
+spec("romareo.plugins.autohelpers.surround")
+spec("romareo.plugins.autohelpers.autotag")
 
 -- movement
-spec "romareo.plugins.movement.whichkey"
-spec "romareo.plugins.movement.nvimtree"
-spec "romareo.plugins.movement.fterm"
-spec "romareo.plugins.movement.toggleterm"
-spec "romareo.plugins.movement.telescope"
-spec "romareo.plugins.movement.harpoon"
-spec "romareo.plugins.movement.neoscroll"
-spec "romareo.plugins.movement.neotab"
-spec "romareo.plugins.movement.searchbox"
+spec("romareo.plugins.movement.whichkey")
+spec("romareo.plugins.movement.nvimtree")
+spec("romareo.plugins.movement.fterm")
+spec("romareo.plugins.movement.toggleterm")
+spec("romareo.plugins.movement.telescope")
+spec("romareo.plugins.movement.harpoon")
+spec("romareo.plugins.movement.neoscroll")
+spec("romareo.plugins.movement.neotab")
+spec("romareo.plugins.movement.searchbox")
 
 -- CPH
-spec "romareo.plugins.cph.cph"
+spec("romareo.plugins.cph.cph")
 
 -- git
-spec "romareo.plugins.git.gitsign"
-spec "romareo.plugins.git.gitfugitive"
-spec "romareo.plugins.git.lazygit"
+spec("romareo.plugins.git.gitsign")
+spec("romareo.plugins.git.gitfugitive")
+spec("romareo.plugins.git.lazygit")
 
-require "romareo.plugins.utils.lazy"
+require("romareo.plugins.utils.lazy")
