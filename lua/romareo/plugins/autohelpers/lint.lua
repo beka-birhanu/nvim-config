30bea8f3a74fe3aa3bdfbd7482927a1b903e6494
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-18 09:48:33 +0300

    fix(lint): provide the correct liters

--- lua/romareo/plugins/autohelpers/lint.lua
+++ lua/romareo/plugins/autohelpers/lint.lua
@@ -6,12 +6,13 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = {
-			javascript = { "eslint" },
-			typescript = { "eslint" },
-			javascriptreact = { "eslint" },
-			typescriptreact = { "eslint" },
-			python = { "ruff" },
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
 			go = { "golangcilint" },
+			python = { "ruff" },
+			c_sharp = { "Roslyn Analyzers" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
