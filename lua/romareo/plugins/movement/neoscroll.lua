f2fdb84faf8fa150456921c552e400475e4d0ba4
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-09-11 19:05:01 +0300

    fix: remove deprecated methods

--- lua/romareo/plugins/movement/neoscroll.lua
+++ lua/romareo/plugins/movement/neoscroll.lua
@@ -23,22 +23,41 @@ function M.config()
 		post_hook = nil, -- Function to run after the scrolling animation ends
 		performance_mode = false, -- Disable "Performance Mode" on all buffers.
 	})
+	local neoscroll = require("neoscroll")
+	local keymap = {
+		["<C-u>"] = function()
+			neoscroll.ctrl_u({ duration = 150 })
+		end,
+		["<C-d>"] = function()
+			neoscroll.ctrl_d({ duration = 150 })
+		end,
+		["<C-b>"] = function()
+			neoscroll.ctrl_b({ duration = 150 })
+		end,
+		["<C-k>"] = function()
+			neoscroll.ctrl_f({ duration = 150 })
+		end,
+		["<C-j>"] = function()
+			neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
+		end,
+		["<C-e>"] = function()
+			neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
+		end,
+		["zt"] = function()
+			neoscroll.zt({ half_win_duration = 250 })
+		end,
+		["zz"] = function()
+			neoscroll.zz({ half_win_duration = 250 })
+		end,
+		["zb"] = function()
+			neoscroll.zb({ half_win_duration = 250 })
+		end,
+	}
 
-	local t = {}
-	-- Syntax: t[keys] = {function, {function arguments}}
-	t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "150" } }
-	t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "150" } }
-	t["<C-k>"] = { "scroll", { "-vim.wo.scroll", "true", "150" } }
-	t["<C-j>"] = { "scroll", { "vim.wo.scroll", "true", "150" } }
-	t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "450" } }
-	t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "450" } }
-	t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
-	t["<C-e>"] = { "scroll", { "0.10", "false", "100" } }
-	t["zt"] = { "zt", { "250" } }
-	t["zz"] = { "zz", { "250" } }
-	t["zb"] = { "zb", { "250" } }
-
-	require("neoscroll.config").set_mappings(t)
+	local modes = { "n", "v", "x" }
+	for key, func in pairs(keymap) do
+		vim.keymap.set(modes, key, func)
+	end
 end
 
 return M
